f4c27f1ebb4e4cb42a0c3c8cd132eab8e6560e35
yashkotadia <yashkotadia@gmail.com>
2020-03-12 20:23:26 -0700

    support for bigtable and maps

--- index/IndexScan.java
+++ index/IndexScan.java
@@ -3,9 +3,9 @@ import global.*;
 import bufmgr.*;
 import diskmgr.*; 
 import btree.*;
-import iterator.*;
-import heap.*; 
+import iterator.*; 
 import java.io.*;
+import BigT.*;
 
 
 /**
@@ -36,17 +36,13 @@ public class IndexScan extends Iterator {
    * @exception IOException from the lower layer
    */
   public IndexScan(
-	   IndexType     index,        
-	   final String  relName,  
-	   final String  indName,  
-	   AttrType      types[],      
-	   short         str_sizes[],     
-	   int           noInFlds,          
-	   int           noOutFlds,         
-	   FldSpec       outFlds[],     
-	   CondExpr      selects[],  
-	   final int     fldNum,
-	   final boolean indexOnly
+	    IndexType     index,        
+	    final String  relName,  
+	    final String  indName,  
+	    String rFilter, 
+      String cFilter,
+      String vFilter,
+      CondExpr selects[]
 	   ) 
     throws IndexException, 
 	   InvalidTypeException,
@@ -54,44 +50,18 @@ public class IndexScan extends Iterator {
 	   UnknownIndexTypeException,
 	   IOException
   {
-    _fldNum = fldNum;
-    _noInFlds = noInFlds;
-    _types = types;
-    _s_sizes = str_sizes;
+    rowFilter = rFilter;
+    columnFilter = cFilter;
+    valueFilter = vFilter;
+    map1 = new Map();
     
-    AttrType[] Jtypes = new AttrType[noOutFlds];
-    short[] ts_sizes;
-    Jtuple = new Tuple();
+    m1_size = map1.size();
     
     try {
-      ts_sizes = TupleUtils.setup_op_tuple(Jtuple, Jtypes, types, noInFlds, str_sizes, outFlds, noOutFlds);
-    }
-    catch (TupleUtilsException e) {
-      throw new IndexException(e, "IndexScan.java: TupleUtilsException caught from TupleUtils.setup_op_tuple()");
-    }
-    catch (InvalidRelation e) {
-      throw new IndexException(e, "IndexScan.java: InvalidRelation caught from TupleUtils.setup_op_tuple()");
-    }
-     
-    _selects = selects;
-    perm_mat = outFlds;
-    _noOutFlds = noOutFlds;
-    tuple1 = new Tuple();    
-    try {
-      tuple1.setHdr((short) noInFlds, types, str_sizes);
+      bgt = new bigT(relName);
     }
     catch (Exception e) {
-      throw new IndexException(e, "IndexScan.java: Heapfile error");
-    }
-    
-    t1_size = tuple1.size();
-    index_only = indexOnly;  // added by bingjie miao
-    
-    try {
-      f = new Heapfile(relName);
-    }
-    catch (Exception e) {
-      throw new IndexException(e, "IndexScan.java: Heapfile not created");
+      throw new IndexException(e, "IndexScan.java: Big Table not created");
     }
     
     switch(index.indexType) {
@@ -133,12 +103,12 @@ public class IndexScan extends Iterator {
    * @exception UnknownKeyTypeException key type unknown
    * @exception IOException from the lower layer
    */
-  public Tuple get_next() 
+  public Map get_next() 
     throws IndexException, 
 	   UnknownKeyTypeException,
 	   IOException
   {
-    RID rid;
+    MID mid;
     int unused;
     KeyDataEntry nextentry = null;
 
@@ -150,95 +120,22 @@ public class IndexScan extends Iterator {
     }	  
     
     while(nextentry != null) {
-      if (index_only) {
-	// only need to return the key 
-
-	AttrType[] attrType = new AttrType[1];
-	short[] s_sizes = new short[1];
-	
-	if (_types[_fldNum -1].attrType == AttrType.attrInteger) {
-	  attrType[0] = new AttrType(AttrType.attrInteger);
-	  try {
-	    Jtuple.setHdr((short) 1, attrType, s_sizes);
-	  }
-	  catch (Exception e) {
-	    throw new IndexException(e, "IndexScan.java: Heapfile error");
-	  }
-	  
-	  try {
-	    Jtuple.setIntFld(1, ((IntegerKey)nextentry.key).getKey().intValue());
-	  }
-	  catch (Exception e) {
-	    throw new IndexException(e, "IndexScan.java: Heapfile error");
-	  }	  
-	}
-	else if (_types[_fldNum -1].attrType == AttrType.attrString) {
-	  
-	  attrType[0] = new AttrType(AttrType.attrString);
-	  // calculate string size of _fldNum
-	  int count = 0;
-	  for (int i=0; i<_fldNum; i++) {
-	    if (_types[i].attrType == AttrType.attrString)
-	      count ++;
-	  } 
-	  s_sizes[0] = _s_sizes[count-1];
-	  
-	  try {
-	    Jtuple.setHdr((short) 1, attrType, s_sizes);
-	  }
-	  catch (Exception e) {
-	    throw new IndexException(e, "IndexScan.java: Heapfile error");
-	  }
-	  
-	  try {
-	    Jtuple.setStrFld(1, ((StringKey)nextentry.key).getKey());
-	  }
-	  catch (Exception e) {
-	    throw new IndexException(e, "IndexScan.java: Heapfile error");
-	  }	  
-	}
-	else {
-	  // attrReal not supported for now
-	  throw new UnknownKeyTypeException("Only Integer and String keys are supported so far"); 
-	}
-	return Jtuple;
-      }
-      
+            
       // not index_only, need to return the whole tuple
-      rid = ((LeafData)nextentry.data).getData();
+      mid = new MID(((LeafData)nextentry.data).getData());
       try {
-	tuple1 = f.getRecord(rid);
+	map1 = bgt.getMap(mid);
       }
       catch (Exception e) {
 	throw new IndexException(e, "IndexScan.java: getRecord failed");
       }
-      
-      try {
-	tuple1.setHdr((short) _noInFlds, _types, _s_sizes);
-      }
-      catch (Exception e) {
-	throw new IndexException(e, "IndexScan.java: Heapfile error");
-      }
-    
-      boolean eval;
-      try {
-	eval = PredEval.Eval(_selects, tuple1, null, _types, null);
-      }
-      catch (Exception e) {
-	throw new IndexException(e, "IndexScan.java: Heapfile error");
-      }
-      
-      if (eval) {
-	// need projection.java
-	try {
-	  Projection.Project(tuple1, _types, Jtuple, perm_mat, _noOutFlds);
-	}
-	catch (Exception e) {
-	  throw new IndexException(e, "IndexScan.java: Heapfile error");
-	}
 
-	return Jtuple;
-      }
+      //System.out.println(map1==null);
+    
+      if(checkFilters(map1.getRowLabel(), rowFilter) && checkFilters(map1.getColumnLabel(), columnFilter) && checkFilters(map1.getValue(), valueFilter)){
+          //System.out.println("Here");
+          return map1;
+      }     
 
       try {
 	nextentry = indScan.get_next();
@@ -272,21 +169,30 @@ public class IndexScan extends Iterator {
       closeFlag = true; 
     }
   }
+
+  // For range filter, we assume that there is no space, eg. [A,Y]
+  private boolean checkFilters(String mapLabel, String filter){
+      if(filter == "" || filter == "*" || (mapLabel.compareTo(filter)==0) )
+        return true;
+      else if(filter.charAt(0) == '['){
+        //System.out.println(filter);
+        String startRange = filter.substring(1, filter.indexOf(','));
+        String endRange = filter.substring(filter.indexOf(',')+1, filter.indexOf(']'));
+        if(mapLabel.compareToIgnoreCase(startRange) >= 0 && mapLabel.compareToIgnoreCase(endRange) <= 0){
+          return true;
+        }
+      }
+      return false; 
+  }
   
-  public FldSpec[]      perm_mat;
   private IndexFile     indFile;
   private IndexFileScan indScan;
-  private AttrType[]    _types;
-  private short[]       _s_sizes; 
+  private bigT          bgt;
+  private Map           map1;
+  private int           m1_size;
   private CondExpr[]    _selects;
-  private int           _noInFlds;
-  private int           _noOutFlds;
-  private Heapfile      f;
-  private Tuple         tuple1;
-  private Tuple         Jtuple;
-  private int           t1_size;
-  private int           _fldNum;       
-  private boolean       index_only;    
-
+  private String        rowFilter;
+  private String        columnFilter;
+  private String        valueFilter;     
 }
 
