2140811e39112248e35cd698bdd6b2048d7d5476
yashkotadia <yashkotadia@gmail.com>
2020-03-31 17:37:27 -0700

    tempory heap file based sorting

--- BatchInsert.java
+++ BatchInsert.java
@@ -31,11 +31,14 @@ public class BatchInsert implements GlobalConst {
             String line = br.readLine();
 
             bigT bigTable = null;
+            bigT tempbt = null;
+            
             try {
                 bigTable = new bigT(bigtableName+"_"+databaseType);
+                tempbt = new bigT(null);
             }
             catch (Exception e) {
-                System.err.println("*** error in creating bigT ***");
+                System.err.println("*** Error in creating/opening bigT ***");
                 e.printStackTrace();
             }
 
@@ -43,16 +46,16 @@ public class BatchInsert implements GlobalConst {
 
                 Map m = new Map();
                 String[ ] attributes = line.split(",");
-                m.setValue(attributes[3]);
+                m.setValue(attributes[2]);
                 m = new Map(m.size());
                 m.setRowLabel(attributes[0]);
                 m.setColumnLabel(attributes[1]);
-                m.setTimeStamp(Integer.parseInt(attributes[2]));
-                m.setValue(attributes[3]);
+                m.setTimeStamp(Integer.parseInt(attributes[3]));
+                m.setValue(attributes[2]);
 
                 try {
-                    bigTable.insertMap(m.returnMapByteArray());
-                    System.out.println("Processed map value: " + attributes[3]);
+                    tempbt.insertMap(m.returnMapByteArray(), false);
+                    System.out.println("Added map timestamp to temporary: " + attributes[3]);
                 }catch (Exception e) {
                     System.err.println("*** error in bigT.insertMap() ***");
                     e.printStackTrace();
@@ -61,6 +64,46 @@ public class BatchInsert implements GlobalConst {
 
                 line = br.readLine();
             }
+
+            if(bigTable.getMapCnt()!=0){
+                Scan scan = bigTable.openScan();
+                MID mid = new MID();
+                while(true) {
+                    Map map1 = new Map();
+                    if((map1 =  scan.getNext(mid)) == null) 
+                        break;
+                    System.out.println("Added Map TS from old to temp: " + map1.getTimeStamp());
+                    tempbt.insertMap(map1.returnMapByteArray(), true);
+                }
+                scan.closescan();
+
+                bigTable.deletebigT();
+                bigTable = new bigT(bigtableName+"_"+databaseType);
+            }
+
+            Stream stream = tempbt.openStream(6, "*", "*", "*");
+            MID mid = new MID();
+            Map prevMap = null;
+            Map rMap;
+            int prevCount = 0;
+            while(true){
+                if((rMap   = stream.getNext(mid)) == null){
+                    break;
+                }
+                if(prevMap!=null && prevMap.getRowLabel().equals(rMap.getRowLabel()) && prevMap.getColumnLabel().equals(rMap.getColumnLabel()) ){
+                    prevCount++;
+                } else {
+                    prevCount = 1;
+                    prevMap = new Map(rMap);
+                }
+                if(prevCount<=3){
+                    System.out.println("Added Map TS from temp to new: " + rMap.getTimeStamp());
+                    bigTable.insertMap(rMap.returnMapByteArray(), true);
+                }
+            }
+            stream.close();
+            tempbt.deletebigT();
+
 
             sysdef.close();
 
