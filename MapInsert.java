389f476f487c8eb512ed13c894772278d7d0079d
yashkotadia <yashkotadia@gmail.com>
2020-03-31 17:13:58 -0700

    changed old BatchInsert to MapInsert

---
+++ MapInsert.java
@@ -0,0 +1,79 @@
+import global.*;
+import index.*;
+import BigT.*;
+import java.io.*;
+import java.lang.*;
+import diskmgr.*;
+import bufmgr.*;
+import btree.*; 
+import catalog.*;
+import iterator.*;
+
+public class BatchInsert implements GlobalConst {
+
+    public static void main(String[] args) {
+
+        //Parsing arguments:
+        if (args.length != 4 || args[0] == "-h")
+        {
+            System.out.println("Enter correct arguments: \nbatchinsert DATAFILENAME TYPE BIGTABLENAME NUMBUFF");
+            return;
+        }
+
+        String dataFileName = args[0], databaseType = args[1], bigtableName = args[2];
+        int numbuffs = Integer.parseInt(args[3]);
+        String dbpath = "/tmp/"+System.getProperty("user.name")+bigtableName+"_"+databaseType; 
+        SystemDefs sysdef = new SystemDefs( dbpath, 20000, numbuffs, "Clock",  Integer.parseInt(databaseType));
+        //Reading input csv file:
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(dataFileName));
+
+            String line = br.readLine();
+
+            bigT bigTable = null;
+            try {
+                bigTable = new bigT(bigtableName+"_"+databaseType);
+            }
+            catch (Exception e) {
+                System.err.println("*** error in creating bigT ***");
+                e.printStackTrace();
+            }
+
+            while (line != null) {
+
+                Map m = new Map();
+                String[ ] attributes = line.split(",");
+                m.setValue(attributes[3]);
+                m = new Map(m.size());
+                m.setRowLabel(attributes[0]);
+                m.setColumnLabel(attributes[1]);
+                m.setTimeStamp(Integer.parseInt(attributes[2]));
+                m.setValue(attributes[3]);
+
+                try {
+                    bigTable.insertMap(m.returnMapByteArray());
+                    System.out.println("Processed map value: " + attributes[3]);
+                }catch (Exception e) {
+                    System.err.println("*** error in bigT.insertMap() ***");
+                    e.printStackTrace();
+                }
+
+
+                line = br.readLine();
+            }
+
+            sysdef.close();
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }catch (InvalidMapSizeException e){
+            System.err.println("*** InvalidMapSize ***");
+            e.printStackTrace();
+        }catch(Exception e){
+            System.err.println("*** error ***");
+            e.printStackTrace();
+        }
+
+}
+
+}
